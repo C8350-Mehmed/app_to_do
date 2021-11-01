08ca6f55ebd73039352234c1b49307de007f6065
C8350-Mehmed <82046540+C8350-Mehmed@users.noreply.github.com>
2021-11-01 15:15:13 +0300

    çalışma devam

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+//Classes name
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH ="lineThrough"
+
+//Show todays date
+
+const options = {weekday :"long", month:"short", day:"numeric"};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
